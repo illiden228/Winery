0aa4b8770c6c4fca7eacbc92ddcd511cdbd4e2dd
Denis Khabibullin <den.khabibullin@gmail.com>
2023-10-07 17:13:43 +0300

    add tools

---
+++ Assets/Scripts/Core/Root.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Core;
+using UnityEngine;
+
+public class Root : BaseMonobehaviour
+{
+    private void Awake()
+    {
+        
+    }
+}
