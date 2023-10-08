2341c2b56077ee587dbf1e6a8bb152f87d427535
Владислав Иванов <hezangames@gmail.com>
2023-10-08 21:55:38 +0300

    fix bug

--- Assets/Scripts/Game/Selectables/PlantPm.cs
+++ Assets/Scripts/Game/Selectables/PlantPm.cs
@@ -2,6 +2,7 @@ using Core;
 using System;
 using Data;
 using Tools.Extensions;
+using UnityEngine;
 
 public class PlantPm : BaseDisposable, IGrowable
 {
@@ -24,7 +25,7 @@ public class PlantPm : BaseDisposable, IGrowable
     private IDisposable _updateGrowthDisposable;
     private PlantStages _currentPlantStage;
     public bool Grown => _currentStep == _ctx.seedling.SproutStageCount;
-    public bool Ripened => _currentStep == _ctx.seedling.RipeStageCount;
+    public bool Ripened => _currentStep == _ctx.seedling.RipeStageCount && _currentPlantStage == PlantStages.FruitsRipened;
 
     public PlantPm(Ctx ctx)
     {
@@ -69,7 +70,7 @@ public class PlantPm : BaseDisposable, IGrowable
                     _currentStep++;
                     _ctx.plantView.UpdatePlantView(_currentStep, Ripened);                    
 
-                    if (Ripened)
+                    if (_currentStep == _ctx.seedling.RipeStageCount)
                     {
                         if (_updateGrowthDisposable != null)
                             _updateGrowthDisposable.Dispose();
@@ -81,7 +82,7 @@ public class PlantPm : BaseDisposable, IGrowable
                 }
             case PlantStages.FruitsRipened:
                 {
-                    _currentStep = 1;
+                    _currentStep = 0;
                     _currentPlantStage = PlantStages.FruitsRipening;
 
                     _updateGrowthDisposable = ReactiveExtensions.RepeatableDelayedCall(_ctx.seedling.FruitRipeTime / _ctx.seedling.RipeStageCount, UpdateGrowth);
@@ -101,6 +102,8 @@ public class PlantPm : BaseDisposable, IGrowable
         SeedlingData grapeData = new SeedlingData { Count = 1, Id = _ctx.seedling.Id, MaxCount = _ctx.seedling.MaxCount, Name = "Grape", Plant = _ctx.seedling.Plant };
 
         callBack?.Invoke(grapeData);
+        _ctx.plantView.UpdatePlantView(0);
+        Debug.Log("Плоды собраны!");
     }
 
     protected override void OnDispose()
